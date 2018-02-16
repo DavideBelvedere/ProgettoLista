4ca6fa498718e94cc12f380bee51b61a1b9cfc62
DavideBelvedere <davide.belvedere@gmail.com>
2018-02-16 09:26:07 +0100

    modifiche utilità p2

--- src/app/videogame.ts
+++ src/app/videogame.ts
@@ -3,14 +3,22 @@ export class VideoGame {
     private title: string;
     private price: number;
     private id: string;
+    private genere: string;
+    private rating: number;
+    private anno: number;
 
 
-    constructor($imgUrl: string = "", $title: string, $price: number, $id: string) {
-        this.imgUrl = $imgUrl;
-        this.title = $title;
-        this.price = $price;
-        this.id = $id;
-    }
+
+	constructor($imgUrl: string, $title: string, $price: number, $id: string, $genere: string="", $rating: number=0, $anno: number=0) {
+		this.imgUrl = $imgUrl;
+		this.title = $title;
+		this.price = $price;
+		this.id = $id;
+		this.genere = $genere;
+		this.rating = $rating;
+		this.anno = $anno;
+	}
+  
 
 
 
